40589b67ebb526f98ae010d5b44ca5ae1d811369
MartyofM <mwangimathewkariuki@gmail.com>
2023-11-28 09:47:30 -0800

    PYYYY

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,10 +1,6 @@
 import sys
 
 def write_art():
-    sys.stderr.write('''
-/\\/\\
-Dora Korpar, 2015-10-19
-''')
-
+    sys.stderr.write('''/\\/\\Dora Korpar, 2015-10-19''')
 write_art()
 sys.exit(1)
