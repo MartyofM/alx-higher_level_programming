7d5af7e85da02837f9a4647101f6870a28991156
MartyofM <mwangimathewkariuki@gmail.com>
2023-11-28 08:50:01 -0800

    Python Yaay

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,10 @@
+import sys
+
+def write_art():
+    sys.stderr.write('''
+/\\/\\
+Dora Korpar, 2015-10-19
+''')
+
+write_art()
+sys.exit(1)
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # alx-higher_level_programming
+Python coding at the start
